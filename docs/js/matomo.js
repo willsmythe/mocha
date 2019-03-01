07761717131106517423e93205e0045365df0408
Christopher Hiller <boneskull@boneskull.com>
2019-03-01 13:21:58 -0800

    add Matomo headers to 11ty and jsdoc; closes #3785
    - use new `headScripts` config per `@mocha/docdash` @ 2.1.0
    - do not send analytics in dev environment
    - add subdomain tracking

---
+++ docs/js/matomo.js
@@ -0,0 +1,21 @@
+// don't trigger analytics for local development environments
+if (document.domain !== 'localhost') {
+  var _paq = _paq || [];
+  // subdomain tracking support, e.g., next.mochajs.org vs. mochajs.org
+  _paq.push(['setDocumentTitle', document.domain + '/' + document.title]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  _paq.push(['trackPageView']);
+  (function() {
+    var u = 'https://mochajs.matomo.cloud/';
+    _paq.push(['setTrackerUrl', u + 'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d = document;
+    var g = d.createElement('script');
+    var s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.defer = true;
+    g.src = u + 'piwik.js';
+    s.parentNode.insertBefore(g, s);
+  })();
+}
